2791837a2522bdd2498c1510df07b7f3ada0d134
manolo <mmartinortiz@gmail.com>
2023-11-18 15:23:22 +0100

    Manage multiple LEDs

--- rgb_led/rgb_led.py
+++ rgb_led/rgb_led.py
@@ -1,21 +1,54 @@
-from machine import Pin, PWM
 from time import sleep_us
 
-led = Pin(25, Pin.OUT)
+from machine import PWM, Pin
 
-led.on()
-frequency = 1000
+power_led = Pin(25, Pin.OUT)
 
-red = PWM(Pin("GP2"), freq=frequency)
-green = PWM(Pin("GP3"), freq=frequency)
-blue = PWM(Pin("GP12"), freq=frequency)
+power_led.on()
+FREQUENCY = 1000
+
+RED = "red"
+GREEN = "green"
+BLUE = "blue"
+COLORS = [RED, GREEN, BLUE]
+
+
+class PwmLed:
+    def __init__(self, red: PWM, green: PWM, blue: PWM) -> None:
+        self.red = red
+        self.green = green
+        self.blue = blue
+
+    def color_duty(self, color: str, duty: int):
+        if color == RED:
+            self.red.duty_u16(duty)
+        if color == GREEN:
+            self.green.duty_u16(duty)
+        if color == BLUE:
+            self.blue.duty_u16(duty)
+
+
+LEDS = [
+    PwmLed(
+        red=PWM(Pin("GP2"), freq=FREQUENCY),
+        green=PWM(Pin("GP3"), freq=FREQUENCY),
+        blue=PWM(Pin("GP12"), freq=FREQUENCY),
+    ),
+    PwmLed(
+        red=PWM(Pin("GP13"), freq=FREQUENCY),
+        green=PWM(Pin("GP14"), freq=FREQUENCY),
+        blue=PWM(Pin("GP15"), freq=FREQUENCY),
+    ),
+]
 
 while True:
-    for color in [red, green, blue]:
+    for color in COLORS:
         for duty in range(65025):
-            color.duty_u16(duty)
+            for led in LEDS:
+                led.color_duty(color=color, duty=duty)
             sleep_us(100)
 
         for duty in range(65025, 0, -1):
-            color.duty_u16(duty)
+            for led in LEDS:
+                led.color_duty(color=color, duty=duty)
             sleep_us(100)
